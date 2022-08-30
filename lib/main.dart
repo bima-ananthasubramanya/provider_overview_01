d2f6a7223ceb90b54d3590d98117f5b2e9e4b19e
Milvik <milvik@Milviks-MacBook-Pro.local>
2022-08-30 17:13:45 +0530

    converting one stateful to many stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -54,57 +54,110 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text('My Home Page',
                   style: TextStyle(color: Colors.black)),
             ),
-            Container(
-              padding: const EdgeInsets.all(30),
-              margin: const EdgeInsets.all(30),
-              color: Colors.pink.shade100,
-              child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  // crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      '$_counter',
-                      style: const TextStyle(fontSize: 30),
-                    ),
-                    ElevatedButton(
-                      onPressed: _incrementCounter,
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                      ),
-                      child: const Text(
-                        'Increment',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ]),
-            ),
-            Container(
-              color: Colors.grey,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(left: 100, right: 100),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    color: Colors.yellow.shade100,
-                    child: Text('$_counter'),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.orange.shade100),
-                    onPressed: _incrementCounter,
-                    child: const Text(
-                      'Sibling',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  )
-                ],
-              ),
-            )
+            CounterA(counter: _counter, incrementCounter: _incrementCounter),
+            Middle(counter: _counter, incrementCounter: _incrementCounter),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  final int counter;
+  final Function() incrementCounter;
+  const Middle({
+    Key? key,
+    required this.counter,
+    required this.incrementCounter,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(left: 100, right: 100),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CounterB(counter: counter),
+          Sibling(incrementCounter: incrementCounter),
+        ],
+      ),
+    );
+  }
+}
+
+class Sibling extends StatelessWidget {
+  final Function() incrementCounter;
+  const Sibling({
+    Key? key,
+    required this.incrementCounter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(primary: Colors.orange.shade100),
+      onPressed: incrementCounter,
+      child: const Text(
+        'Sibling',
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+}
+
+class CounterB extends StatelessWidget {
+  final int counter;
+  const CounterB({
+    Key? key,
+    required this.counter,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      color: Colors.yellow.shade100,
+      child: Text('$counter'),
+    );
+  }
+}
+
+class CounterA extends StatelessWidget {
+  final int counter;
+  Function() incrementCounter;
+  CounterA({
+    Key? key,
+    required this.counter,
+    required this.incrementCounter,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(30),
+      margin: const EdgeInsets.all(30),
+      color: Colors.pink.shade100,
+      child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              '$counter',
+              style: const TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(
+              onPressed: incrementCounter,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              child: const Text(
+                'Increment',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ]),
     );
   }
 }
